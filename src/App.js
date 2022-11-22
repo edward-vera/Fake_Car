e848cc582777d99c262d32aa9fdbc6bfe84ec4dd
edward-vera <applesawced420@gmail.com>
2022-11-21 23:34:50 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navigation from './components/Navigation'
 import './App.css'
+import Router from './Router';
 // Write imports for Router & BrowserRouter here //
 
 function App() {
   return (
-    <Navigation />
+    <div>
+      <Navigation />
+      <Router />
+    </div>
   );
 }
 
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
+import React from 'react';
+import cars from '../cars.json';
+import {useParams} from "react-router-dom";
+import '../App.css';
+import { Card, CardContent, CardActions, Divider } from '@mui/material'
 
 // import MUI components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    const {id} = useParams();
+
+    const car = cars.find((car) => car.id === +id);
+    console.log(car);
+
     return (
-        <h1>A specific car</h1>
+        <div className='car-info'>
+            <Card className="card-info">
+                <CardContent className='car-card'>
+                    <div>
+                        <h1>{car.Name}</h1>
+                    </div>
+                    <div>
+                        <p>Id: {car.id}</p>
+                        <p>Name: {car.Name}</p>
+                        <p>Miles_per_Gallon: {car.Miles_per_Gallon}</p>
+                        <p>Cylinders: {car.Cylinders}</p>
+                        <p>Displacement: {car.Displacement}</p>
+                        <p>Horsepower: {car.Horsepower}</p>
+                        <p>Weight: {car.Weight_in_lbs}</p>
+                        <p>Acceleration: {car.Acceleration}</p>
+                        <p>Year: {car.Year}</p>
+                        <p>Origin: {car.Origin}</p>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
 
